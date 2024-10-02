fb0c4cea10523a4ebbc64e4661b898a848d12357
Rebeca <rebecasnascimento018@gmail.com>
2024-10-01 21:09:25 -0400

    Mudando função do js, atualização do css e html

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function zoomImage(element) {
-    element.style.transform = "scale(1.1)";
-    element.style.transition = "transform 0.3s ease";
+function blurImage(element) {
+    element.style.filter = "blur(5px)";
+    element.style.transition = "filter 0.3s ease";
 }
 
-function resetImage(element) {
-    element.style.transform = "scale(0.9)";
+function resetBlur(element) {
+    element.style.filter = "blur(0px)";
 }
